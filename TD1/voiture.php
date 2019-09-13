a754ebfc6254b66f9e08e0824bdf010385c70856
Clement Galzi <clementgalzi@etu.umontpellier.fr>
2019-09-13 14:21:08 +0200

    ajout voiture.php

---
+++ TD1/voiture.php
@@ -0,0 +1,53 @@
+<?php
+class Voiture {
+   
+  private $marque;
+  private $couleur;
+  private $immatriculation;
+      
+  // un getter      
+  public function getMarque() {
+       return $this->marque;  
+  }
+     
+  // un setter 
+  public function setMarque($marque2) {
+       $this->marque = $marque2;
+  }
+
+  public function getImmatriculation() {
+       return $this->immatriculation;  
+  }
+     
+  // un setter 
+  public function setImmmatriculation($immatriculation2) {
+       if  (strlen($immatriculation2)>8){
+          $this->immatriculation = $immatriculation2;
+       }
+       
+  }
+
+  public function getCouleur() {
+       return $this->couleur;  
+  }
+     
+  // un setter 
+  public function setCouleur($couleur2) {
+       $this->couleur = $couleur2;
+  }
+      
+  // un constructeur
+  public function __construct($m, $c, $i)  {
+   $this->marque = $m;
+   $this->couleur = $c;
+   $this->immatriculation = $i;
+  } 
+           
+  // une methode d'affichage.
+  public function afficher() {
+    return "<br\> Voiture this->immatriculation de marque this->marque (couleur this->couleur) <br/>";
+    
+  }
+}
+?>
+
