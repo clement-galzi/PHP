3df6266e2476cbe8f4d68c572949f3bc0cf31b46
clement-galzi <clementgalzi@gmail.com>
2019-09-20 15:02:04 +0200

    fin ex6

--- TD2/lireVoiture.php
+++ TD2/lireVoiture.php
@@ -1,4 +1,8 @@
 <?php 
-	require_once 'Model.php'
-
+	require_once 'Model.php';
+    $rep = Model::$pdo->query("SELECT * FROM voiture");
+    $tab_obj = $rep->fetchAll(PDO::FETCH_OBJ);
+    foreach ($tab_obj as $obj){
+        echo "immatriculation : $obj->immatriculation, couleur : $obj->couleur, marque : $obj->marque </br>";
+    }
 ?>
